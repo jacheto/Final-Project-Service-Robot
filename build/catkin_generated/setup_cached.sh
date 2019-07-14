eb3e1dec449a96dcf2b1a51e20e2ad82a11f7386
Felipe Jacheto <felipe.jacheto@gmail.com>
2019-07-14 11:44:47 -0300

    primeiro commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/felipe/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/home/felipe/mybot_ws/devel/lib"
+export PKG_CONFIG_PATH="/home/felipe/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig:/home/felipe/mybot_ws/devel/lib/pkgconfig"
+export PWD="/home/felipe/roboserv_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/felipe/roboserv_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY="/home/felipe/roboserv_ws/devel/share/maven"
+export ROS_MAVEN_PATH="/home/felipe/roboserv_ws/devel/share/maven:$ROS_MAVEN_PATH"
+export ROS_PACKAGE_PATH="/home/felipe/roboserv_ws/src:$ROS_PACKAGE_PATH"
---
+++ build/roboserv_simulation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/felipe/roboserv_ws/src/roboserv_simulation
+# Build directory: /home/felipe/roboserv_ws/build/roboserv_simulation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
