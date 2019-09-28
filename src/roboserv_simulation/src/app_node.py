17d147320422c5d4f04338cb2453988fd580cdfa
Felipe Jacheto <felipe.jacheto@gmail.com>
2019-09-28 14:10:30 -0300

    atualizadasso

--- src/roboserv_simulation/src/app_node.py
+++ src/roboserv_simulation/src/app_node.py
@@ -14,37 +14,86 @@ import math
 import os
 import shutil
 import json
+import time
 import requests
+import subprocess
+import shlex
+import sys
+import signal
+import psutil
 from time import sleep
 from six.moves import urllib
 from geometry_msgs.msg import Twist
 
-address = 'http://192.168.1.2:5010' #rospy.get_param('~server_address')
+def kill_child_processes(parent_pid, sig=signal.SIGTERM):
+    try:
+        parent = psutil.Process(parent_pid)
+        print(parent)
+    except psutil.NoSuchProcess:
+        print("parent process not existing")
+        return
+    children = parent.children(recursive=True)
+    print(children)
+    for process in children:
+        print("try to kill child: " + str(process))
+        process.send_signal(sig)
 
-def post(route, data):    
-    requests.post(url = address + '/' + route, data=json.dumps(data))
-    print("post de '" + str(data) + "' em " + route)
+class Roscore(object):
+    """
+    roscore wrapped into a subprocess.
+    Singleton implementation prevents from creating more than one instance.
+    """
+    __initialized = False
+    def __init__(self):
+        if Roscore.__initialized:
+            raise Exception("You can't create more than 1 instance of Roscore.")
+        Roscore.__initialized = True
+    def run(self, command_list):
+        try:
+            self.roscore_process = subprocess.Popen(command_list)
+            self.roscore_pid = self.roscore_process.pid  # pid of the roscore process (which has child processes)
+        except OSError as e:
+            sys.stderr.write('roscore could not be run')
+            raise e
+    def terminate(self):
+        print("try to kill child pids of roscore pid: " + str(self.roscore_pid))
+        kill_child_processes(self.roscore_pid)
+        self.roscore_process.terminate()
+        self.roscore_process.wait()  # important to prevent from zombie process
+        Roscore.__initialized = False
 
-def get(route):
-    valor = json.loads(urllib.request.urlopen(address + '/' + route).read())
-    print("get de '" + str(valor) + "' em " + route)
-    return valor
+
+
+def post(chave, valor):
+    address = "http://10.0.0.2:5010"
+    valor_json = json.dumps({'valor': valor, 'timestamp':time.time()})
+    requests.post(url = address + "/post_data?chave=" + chave, data=valor_json)
+
+def get(chave, get_timestamp=False):
+    address = "http://10.0.0.2:5010"
+    valor_json = urllib.request.urlopen(address + '/get_data?chave=' + chave).read()
+    if valor_json == "":
+        return ""
+    valor_dict = json.loads(valor_json)
+    if get_timestamp:
+        return valor_dict
+
 
 def loop():
     
     # Avisa que o PC ligou
-    post("post_PC_on", {"PC_on":True})
+    post("pc_on", True)
 
     # Verifica se o app foi aberto
     app_on = False
     while not app_on:
-        app_on = get('get_App_on')
+        app_on = get('app_on')
         print("aguardando app")
         sleep(0.1)
 
     # Inicia modo escolha de mapa
     navigation_mode = 0
-    post("post_navigation_mode", {"navigation_mode":navigation_mode})
+    post('navigation_mode', navigation_mode)
 
     # Escolha dos mapas
     valid_map = False
@@ -58,18 +107,18 @@ def loop():
         for subdir, dirs, files in os.walk(directory):
             maps_list.append(dirs[0])
         
-        post("post_maps_list", {"maps_list":maps_list})
+        post('maps_list', maps_list)
 
         map_name = ""
         while map_name == "":
-            map_name = get("get_map_name")
+            map_name = get('map_name')
             print("aguardando mapa")
             sleep(0.1)
 
         if map_name.endswith('_del'):
             shutil.rmtree(directory + '/' + map_name.replace('_del', ''))
             apagou_mapa = True
-            post("post_apagou_mapa", {"post_apagou_mapa":apagou_mapa})
+            post('apagou_mapa', True)
         else:
             valid_map = True
     
@@ -89,17 +138,22 @@ def loop():
     
     print("BOOM")
 
-    
     if not os.path.isdir(map_directory):
             os.mkdir(map_directory)
-
     
-
     # inicia ROS com os parâmetros map_name e mapping
+
+    roscore = Roscore()
+    
+    command_list = ['roslaunch', 'roboserv_simulation', 'roboserv_real.launch', 'map_name="' + map_name + '"', 'mapping="' + mapping + '"']
+    #roscore.run(command_list)
+    #time.sleep(20)
+    #roscore.terminate()
 
     # verificar se o ROS já está aberto e rodando
 
-    post("post_navigation_mode", {"navigation_mode":navigation_mode})
+
+    post('navigation_mode',navigation_mode)
 
 
 def begin():
--- src/roboserv_simulation/src/tst3.py
+++ src/roboserv_simulation/src/tst3.py
@@ -6,23 +6,77 @@ import requests
 import json
 import time
 
+
+
+
+import subprocess
+import shlex
+import sys
+import signal
+import psutil
+
+def kill_child_processes(parent_pid, sig=signal.SIGTERM):
+    try:
+        parent = psutil.Process(parent_pid)
+        print(parent)
+    except psutil.NoSuchProcess:
+        print("parent process not existing")
+        return
+    children = parent.children(recursive=True)
+    print(children)
+    for process in children:
+        print("try to kill child: " + str(process))
+        process.send_signal(sig)
+
+class Roscore(object):
+    """
+    roscore wrapped into a subprocess.
+    Singleton implementation prevents from creating more than one instance.
+    """
+    __initialized = False
+    def __init__(self):
+        if Roscore.__initialized:
+            raise Exception("You can't create more than 1 instance of Roscore.")
+        Roscore.__initialized = True
+    def run(self, command_list):
+        try:
+            self.roscore_process = subprocess.Popen(command_list)
+            self.roscore_pid = self.roscore_process.pid  # pid of the roscore process (which has child processes)
+        except OSError as e:
+            sys.stderr.write('roscore could not be run')
+            raise e
+    def terminate(self):
+        print("try to kill child pids of roscore pid: " + str(self.roscore_pid))
+        kill_child_processes(self.roscore_pid)
+        self.roscore_process.terminate()
+        self.roscore_process.wait()  # important to prevent from zombie process
+        Roscore.__initialized = False
+
+
+
+
+
+
 def post(chave, valor):
-    address = "http://192.168.1.2:5010"
-    url = address + "/post_data?chave=" + chave
-    valor_json = json.dumps({'valor': json.dumps(valor), 'timestamp':time.time()})
-    requests.post(url = url, data=valor_json)
+    address = "http://10.0.0.2:5010"
+    valor_json = json.dumps({'valor': valor, 'timestamp':time.time()})
+    requests.post(url = address + "/post_data?chave=" + chave, data=valor_json)
 
 def get(chave, get_timestamp=False):
-    address = "http://192.168.1.2:5010"
-    url = address + "/post_data?name=" + chave
-    valor = urllib.request.urlopen(address + '/get_data?chave=' + chave).read()
-    if valor == "":
+    address = "http://10.0.0.2:5010"
+    valor_json = urllib.request.urlopen(address + '/get_data?chave=' + chave).read()
+    if valor_json == "":
         return ""
-    
-    valor_json = json.loads(valor)
+    valor_dict = json.loads(valor_json)
     if get_timestamp:
-        return valor_json['timestamp']
+        return valor_dict['timestamp']
     
-    return json.loads(valor_json['valor'])
+    return valor_dict['valor']
 
-print(get('server_on'))
+
+
+roscore = Roscore()
+command_list = ['roslaunch', 'roboserv_simulation', 'roboserv_real.launch']
+roscore.run(command_list)
+time.sleep(20)
+roscore.terminate()
